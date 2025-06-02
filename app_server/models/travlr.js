78b31625f0bccb4f9a3c6858fa95976b418f8345
Braden0006 <96596757+Braden0006@users.noreply.github.com>
2025-06-01 23:22:02 -0600

    Module 4 completed baseline

--- app_server/models/travlr.js
+++ app_server/models/travlr.js
@@ -9,7 +9,8 @@ const tripSchema = new mongoose.Schema({
   resort: { type: String, required: true },
   perPerson: { type: String, required: true },
   image: { type: String, required: true },
-  description: { type: String, required: true }
+  description1: { type: String, required: true },
+  description2: {type: String, required: true}
 });
 
 const Trip = mongoose.model('trips', tripSchema);
